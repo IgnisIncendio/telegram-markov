ed2624ec8a3cc041478651e2ce2ccb054cd9d170
Ignis <IgnisIncendio@protonmail.com>
2017-07-26 21:59:25 +0800

    Add user friendly training and generation

--- view.js
+++ view.js
@@ -4,19 +4,22 @@ const telegraf = require("telegraf")
 const randomItemInArray = require("random-item-in-array")
 const isEmpty = require("is-empty")
 const loadJsonFile = require("load-json-file")
+const whitespaceSplit = require("whitespace-split")
+const lowerCase = require("lower-case")
 // Config
-
+const config = loadJsonFile.sync("config.json")
 // Bot
-const bot = new telegraf()
+const bot = new telegraf(config.apiKey)
 // UI Elements
 const logText = $("#log");
-
+// Markov chain
 const chain = {
   chain: {},
   train: function(items) {
     let prevItem = null
     for (let i = 0; i < items.length; i++) {
       const item = items[i]
+      // Add this item to prev item's array
       if (this.chain[prevItem] == undefined) this.chain[prevItem] = []
       this.chain[prevItem].push(item)
       prevItem = item
@@ -26,21 +29,34 @@ const chain = {
     const items = []
     let prevItem = null
     while (items.length < maxLength) {
-      const possibleNewItems = this.chain[prevItem]
-      if (isEmpty(possibleNewItems)) break
-      const chosenNewItem = randomItemInArray(possibleNewItems)
-      items.push(chosenNewItem)
-      prevItem = chosenNewItem
+      const possibleThisItems = this.chain[prevItem]
+      // Stop if dead end
+      if (isEmpty(possibleThisItems)) break
+      // Choose current item from the possible items and push to result
+      const chosenThisItem = randomItemInArray(possibleThisItems)
+      items.push(chosenThisItem)
+      prevItem = chosenThisItem
     }
     return items
   }
 }
-
+// Words
+const normalize = function(text) {
+  return lowerCase(text)
+}
+const train = function(text) {
+  const textSplit = whitespaceSplit(normalize(text))
+  chain.train(textSplit)
+}
+const generate = function() {
+  const generated = chain.generate(config.maxLength)
+  return generated.join(" ")
+}
+// Log
 const log = function(message) {
   logText.val(logText.val() +
     "[Log] " + message + "\n")
 }
-
 // Start bot
 bot.command("/generate", (ctx) => ctx.reply("Hey there!!"))
 bot.startPolling()
